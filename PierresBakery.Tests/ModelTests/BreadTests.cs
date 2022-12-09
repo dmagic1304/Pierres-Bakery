c1c9faad39698469d00e30c6d014fd7f80694763
Dominik Magic <dominikmagic5@gmail.com>
2022-12-09 09:18:25 -0800

    Test 1 passed, Test 2 added

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,13 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+    
+    [TestMethod]
+    public void GetInput_ItStoreUserInputAsProperty_Int()
+    {
+      int userInput = 5;
+      Bread newBread = new Bread(userInput);
+      Assert.AreEqual(userInput, newBread.Quantity());
+    }
   }
 }
