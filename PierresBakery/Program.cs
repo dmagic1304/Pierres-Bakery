1002a83c9f6dd9f0169c0451ab7c4170d639a358
Dominik Magic <dominikmagic5@gmail.com>
2022-12-09 16:14:21 -0800

    It will display the promotion offer and store user input

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -10,11 +10,18 @@ namespace PierresBakery
       
       Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("\n Welcome to Pierre's Bakery!!\n*****************************\n");
+      Console.ResetColor();
       Console.WriteLine("Here is our offer for today:");
       Console.WriteLine("--------------------------------------------------------------------------");
       Console.WriteLine(">> Fresh bread for only $5 per loaf with a special offer of BUY 2 GET 1 FREE!!");
       Console.WriteLine(">> Tasty Pastry at $2 for one with special price of 5$ FOR 3!!");
       Console.WriteLine("--------------------------------------------------------------------------");
+
+      Console.WriteLine("How many loafs of brad would you like to buy?");
+      int breadQuantity = int.Parse(Console.ReadLine());
+      Console.WriteLine("And how many pastries can I get for you?");
+      int pastryQuantity = int.Parse(Console.ReadLine());
+
     }
   }
 }
