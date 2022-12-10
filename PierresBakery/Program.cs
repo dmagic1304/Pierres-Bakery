beb1f8a4f454569130fe03669b5f714f881794ef
Dominik Magic <dominikmagic5@gmail.com>
2022-12-09 16:04:24 -0800

    WIP: working on console display

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -5,6 +5,16 @@ namespace PierresBakery
 {
   public class Program
   {
-    
+    static void Main()
+    {
+      
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("\n Welcome to Pierre's Bakery!!\n*****************************\n");
+      Console.WriteLine("Here is our offer for today:");
+      Console.WriteLine("--------------------------------------------------------------------------");
+      Console.WriteLine(">> Fresh bread for only $5 per loaf with a special offer of BUY 2 GET 1 FREE!!");
+      Console.WriteLine(">> Tasty Pastry at $2 for one with special price of 5$ FOR 3!!");
+      Console.WriteLine("--------------------------------------------------------------------------");
+    }
   }
 }
